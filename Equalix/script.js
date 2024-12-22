b18aedbfe3f84e9a3e88f4556974846642eb0bed
Fujiirkfc <fujiiken026@gmail.com>
2024-12-22 23:11:37 +0900

    Put the number input and the highscore value when gets a correct answer

--- Equalix/script.js
+++ Equalix/script.js
@@ -1,8 +1,46 @@
 'use-strict';
-const numbers = [... Array(4)].map(e=> Math.floor(Math.random() * 100));
-const randomNumber = Math.trunc(Math.random() * 100) + 1;
-// for (i = 0; i < numbers.length; i++){
-//     numbers[i] = randomNumber;
-// }
-// console.log(randomNumber);
-document.querySelector('.number').textContent = numbers;
+let score = 30;
+let highscore = 0;
+const numbers = [... Array(4)].map(e=> Math.floor(Math.random() * 100) + 1);
+
+function displayMessage(message){
+    document.querySelector('.message').textContent = message;
+}
+
+function getRandomOperator() {
+    const randomOperator = Math.floor(Math.random() * 2) + 1;
+    switch(randomOperator) {
+        case 1:
+            return '+';
+        case 2:
+            return '-';
+    }
+}
+
+function getRandomNumber(numbers){
+    const randomIndex = Math.floor(Math.random() * numbers.length);
+    return numbers[randomIndex];
+}
+
+function displayGame(){
+document.querySelector('.number').textContent = getRandomNumber(numbers) + getRandomOperator() + getRandomNumber(numbers);
+}
+
+displayGame();
+
+document.querySelector('.check').addEventListener('click', function(){
+    const correctAnswer = Number(eval(document.querySelector('.number').textContent));
+    const answer = Number(document.querySelector('.answer').value);
+    console.log(answer);
+    console.log(correctAnswer);
+    if(answer === correctAnswer){
+        displayMessage('Correct answer! congratulations');
+        highscore += score;
+        document.querySelector('.highscore').textContent = highscore;
+        displayGame();
+    }
+    else{
+            displayMessage('Wrong, not a number');
+    }
+});
+// resultado da operacao: eval(document.querySelector('.number').textContent));
